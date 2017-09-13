7b818eba6f4907f1bbab1d80a6931017a7645e0e
Michael Doroff <michael_doroff@yahoo.com>
2017-09-12 19:28:59 -0500

    Initial Commit

---
+++ iCate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCate
+//
+//  Created by Michael Doroff on 9/12/17.
+//  Copyright © 2017 Michael Doroff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
