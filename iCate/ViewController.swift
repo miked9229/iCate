98618e207bb8f0f8427f9ebe0be54f24789a671f
Michael Doroff <michael_doroff@yahoo.com>
2017-09-17 20:41:04 -0500

    Added ability to get locations as well as label for iCate

--- iCate/ViewController.swift
+++ iCate/ViewController.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        getLocationUpdate()
+
+    
+    }
+    
+    
+    func getLocationUpdate() {
+        // Create a location manager object
+        self.locationManager = CLLocationManager()
+        
+        // Set the delegate
+       
+        
+        // Request location authorization
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        print(locationManager.location?.coordinate.latitude)
+        print(locationManager.location?.coordinate.longitude)
+        
+        // Start location updates
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    
+    func getQuickLocationUpdate() {
+        // Request location authorization
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        // Request a location update
+        print(self.locationManager.requestLocation())
+        // Note: requestLocation may timeout and produce an error if authorization has not yet been granted by the user
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
