d6b194a2f301aca6c57e63430f38ef0c63c3fb20
Michael Doroff <michael_doroff@yahoo.com>
2017-09-21 19:15:59 -0500

    Added LoginViewController

---
+++ iCate/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  iCate
+//
+//  Created by Michael Doroff on 9/21/17.
+//  Copyright © 2017 Michael Doroff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class LoginViewController {}
